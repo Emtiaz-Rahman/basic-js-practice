7b11f22d969a5db38c58ef26020935c2e181a33b
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-01-28 11:13:30 +0600

    index part

---
+++ arry-index.js
@@ -0,0 +1,21 @@
+var bookPages = ['battery', 'mobile', 'latptop', 'mouse', 'sunglass'];
+
+var mouseIndex = bookPages.indexOf('mouse');
+console.log(mouseIndex);
+
+var numbrs = [45, 50, 12, 58, 13, 100, 60, 47, 46, 25, 27, 19,];
+var index = numbrs.indexOf(200);
+console.log(index);
+
+var thirdIndex = bookPages[4];
+
+bookPages[3] = 80;
+console.log(bookPages);
+
+var bookList = ['bengali', 'english', 'math', 'science', 'highermath', 'biology'];
+var bookposition = bookList.indexOf('highermath');
+console.log(bookposition);
+var thirdIndex = bookList[3];
+console.log(thirdIndex);
+bookList[4] = 'computer science'
+console.log(bookList);
