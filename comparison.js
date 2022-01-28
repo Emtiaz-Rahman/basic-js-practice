254e4bba19b606f9a34a983523cb3595736cdbd7
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-01-28 14:44:24 +0600

    comparison

---
+++ comparison.js
@@ -0,0 +1,8 @@
+console.log(5 > 6);
+console.log(5 < 6);
+console.log(5 == 6);
+console.log(5 != 5);
+var myLove = 100;
+var yourLove = 100;
+console.log(myLove == yourLove);
+console.log(myLove != yourLove);
